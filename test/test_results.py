a10815429f8548864f78c17d06a2e4bcda826096
santicar1809 <santicar1809@gmail.com>
2024-08-13 20:02:41 -0500

    test

--- test/test_results.py
+++ test/test_results.py
@@ -5,29 +5,26 @@ from tensorflow.keras.models import load_model
 import joblib
 
 def test_main():
-    features_test=pd.read_csv('./test/files/features_test.csv')
-    target_test=pd.read_csv('./test/files/target_test.csv')
-    models_name=['lr','xg','lgbm','rf','cat']
-    results=[]
+    next_hr_features={'year':[2018],'month':[9],'day':[1],'hour':[0],'lag_1':[205.0],'lag_2':[223.0],'lag_3':[159.0],'lag_4':[136.0],'rolling_mean':[170.750000]}
+    next_hr_features=pd.DataFrame(next_hr_features)
+    features_test=next_hr_features
+    models_name=['lr','xg','lgbm','rf','cat','dt']
+    list_pred=[]
     for model in models_name:    
         new_model = joblib.load(f'./files/modeling_output/model_fit/best_random_{model}.joblib')
-        prob = new_model.predict_proba(features_test)[:, 1]
-        roc_auc = roc_auc_score(target_test,prob)
-        results.append([model,roc_auc])
-        
-    results_df = pd.DataFrame(results, columns=['model','test_score'])
-    
+        pred_next_hour = new_model.predict(features_test)
+        list_pred.append(pred_next_hour)
+    results_df = pd.DataFrame(list_pred, columns=['test_score'],index=models_name)
+    print(results_df)
     #model_nn = joblib.load(f'./files/modeling_output/model_fit/best_random_nn.joblib')
     model_nn = load_model('./files/modeling_output/model_fit/best_random_nn.h5')
-    proba=model_nn.predict(features_test).ravel()
-    roc_auc_nn=roc_auc_score(target_test,proba)
-    results_n=['Keras',roc_auc_nn]
-    results_n_df = pd.DataFrame({'model':[results_n[0]],'test_score':[results_n[1]]})
+    predict=model_nn.predict(features_test)
+    results_n_df = pd.DataFrame({'test_score':[predict]},index=['Keras'])
     final_rev = pd.concat([results_df,results_n_df])
-    final_rev.to_csv('./files/modeling_output/reports/model_test_report.csv',index=False)
-    print(results_n_df)
+    final_rev.to_csv('./files/modeling_output/reports/model_test_report.csv',index=True)
+
     return final_rev
-    #return results_df
+    
 results_test = test_main()
 
 print(results_test)
